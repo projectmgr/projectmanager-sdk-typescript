9e2484abc8682ce3b9845d989a9d4a450bff6f6a
Ted Spence <ted@spence.net>
2024-08-19 08:02:15 -0700

    Release 115 (#14)
    * Release 115
    
    # Patch notes for 115.0.4328
    
    These patch notes summarize the changes from version 114.0.4169.
    
    Changes to data models:
    * NotificationDto: Added new field `id`
    * NotificationDto: Removed field `notificationId`
    * NotificationDto: Removed field `businessId`
    * NotificationDto: Removed field `userId`
    * ResourcesCreateDto: Added new field `projectIds`
    * RiskExportSettingsDto: Added new field `timeZoneOffset`
    * RiskExportSettingsDto: Added new field `dueDateFilter`
    * RiskExportSettingsDto: Added new field `priorityFilter`
    * RiskExportSettingsDto: Added new field `progressFilter`
    * RiskExportSettingsDto: Added new field `assignees`
    * RiskExportSettingsDto: Added new field `tags`
    * TaskDto: Added new field `theme`
    
    * Run npm audit fix

---
+++ src/models/ExportDueDateFilterDto.ts
@@ -0,0 +1,49 @@
+/**
+ * ProjectManager API for TypeScript
+ *
+ * (c) 2023-2024 ProjectManager.com, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author     ProjectManager.com <support@projectmanager.com>
+ * @copyright  2023-2024 ProjectManager.com, Inc.
+ * @link       https://github.com/projectmgr/projectmanager-sdk-typescript
+ */
+
+
+/**
+ * Due Date Filter Settings
+ */
+export type ExportDueDateFilterDto = {
+
+  /**
+   * Include items without a due date
+   */
+  includeNoDueDate: boolean | null;
+
+  /**
+   * Include items with a due date in the past
+   */
+  includeOverdue: boolean | null;
+
+  /**
+   * Include items with a due date of today
+   */
+  includeToday: boolean | null;
+
+  /**
+   * Include items due within the current week
+   */
+  includeThisWeek: boolean | null;
+
+  /**
+   * Include items due within the next week
+   */
+  includeNextWeek: boolean | null;
+
+  /**
+   * Include items due later
+   */
+  includeLater: boolean | null;
+};
