5893c2dd5db24676a123d49d8b8261029bb21391
tspence <1933486+tspence@users.noreply.github.com>
2024-11-24 00:30:23 +0000

    # Patch notes for 122.0.216
    These patch notes summarize the changes from version 122.0.214.
    
    * Minor documentation changes, bugfixes, and performance improvements.

--- src/ProjectManagerClient.ts
+++ src/ProjectManagerClient.ts
@@ -8,7 +8,7 @@
  *
  * @author     ProjectManager.com <support@projectmanager.com>
  * @copyright  2023-2024 ProjectManager.com, Inc.
- * @version    122.0.214
+ * @version    122.0.216
  * @link       https://github.com/projectmgr/projectmanager-sdk-typescript
  */
 
@@ -82,7 +82,7 @@ export class ProjectManagerClient {
 
   // The URL of the environment we will use
   private readonly serverUrl: string;
-  private readonly version: string = "122.0.214";
+  private readonly version: string = "122.0.216";
   private bearerToken: string | null = null;
   private sdkName = "TypeScript";
   private appName: string | null = null;
